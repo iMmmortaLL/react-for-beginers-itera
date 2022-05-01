1e616bfb39e7b332231d712b4c4ed213288cc796
Vitalii <vitalijkopac00@gmail.com>
2022-05-01 23:49:54 +0300

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import info from "./info.json";
+
+const localData = info.Data;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello React</h1>
+        <ul>
+            {localData.map(s => (<li>{s}</li>))}
+        </ul>
     </div>
   );
 }
+
+
 
 export default App;
